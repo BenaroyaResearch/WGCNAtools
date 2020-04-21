2b0e7278665b3adc90c89f34fb803fb1b68440a1
mjdufort <matt.dufort@gmail.com>
2020-04-21 16:57:57 -0700

    Remove quartz window plotting from plot_softpowers

--- R/plot_softpowers.r
+++ R/plot_softpowers.r
@@ -6,16 +6,12 @@
 #' @param powers A vector of soft thresholding powers; passed to \code{pickSoftThreshold}
 #' @param verbose Integer level of verbosity; passed to \code{pickSoftThreshold}
 #' @export
-#' @usage \code{pickSoftThrehold(counts, powers = c(c(1:10), seq(from = 12, to=20, by=2)), verbose=5))}
+#' @usage \code{plot_softpowers(counts, powers = c(c(1:10), seq(from = 12, to=20, by=2)), verbose=5))}
 plot_softpowers <- function(counts, powers = c(c(1:10), seq(from = 12, to=20, by=2)), verbose=5) {
-  
+
   # Call the network topology analysis function
   sft.tmp <- WGCNA::pickSoftThreshold(counts, powerVector = powers, verbose = verbose)
-  
-  # Plot the results:
-  quartz(w=9,h=5)
-  par(mfrow = c(1,2));
-  
+
   # Scale-free topology fit index as a function of the soft-thresholding power
   plot(sft.tmp$fitIndices[,1],
        -sign(sft.tmp$fitIndices[,3])*sft.tmp$fitIndices[,2],
@@ -26,7 +22,7 @@ plot_softpowers <- function(counts, powers = c(c(1:10), seq(from = 12, to=20, by
        labels=powers,cex=0.9,col="red");
   # this line corresponds to using an R^2 cut-off of h
   abline(h=0.90,col="red")
-  
+
   # Mean connectivity as a function of the soft-thresholding power
   plot(sft.tmp$fitIndices[,1], sft.tmp$fitIndices[,5],
        xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
