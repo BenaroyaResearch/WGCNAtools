a5649efb7cf20a28bb947bc3e2cb0d116a70911c
Matt Dufort <matt.dufort@gmail.com>
2024-10-08 10:30:20 -0700

    Add networkType argument to plot_softpowers. Readability and style updates.

--- R/plot_softpowers.r
+++ R/plot_softpowers.r
@@ -3,29 +3,33 @@
 #' Calculate and plot diagnostics for a range of values of the soft thresholding power, as described
 #' in the online WGCNA tutorials. It is a wrapper for WGCNA::pickSoftThreshold.
 #' @param counts A matrix or data frame of counts, with genes in columns and samples in rows
-#' @param powers A vector of soft thresholding powers; passed to \code{pickSoftThreshold}
-#' @param verbose Integer level of verbosity; passed to \code{pickSoftThreshold}
+#' @param powers A vector of soft thresholding powers; passed to \code{WGCNA::pickSoftThreshold}
+#' @param networkType Character value, the network type, passed to \code{WGCNA::pickSoftThreshold}. Allowed values are (unique abbreviations of) "unsigned", "signed", "signed hybrid". See \code{WGCNA::adjacency}
+#' @param verbose Integer level of verbosity; passed to \code{WGCNA::pickSoftThreshold}
+#' @importFrom graphics abline text
 #' @export
-#' @usage \code{plot_softpowers(counts, powers = c(c(1:10), seq(from = 12, to=20, by=2)), verbose=5))}
-plot_softpowers <- function(counts, powers = c(c(1:10), seq(from = 12, to=20, by=2)), verbose=5) {
+#' @usage plot_softpowers(counts, powers = c(1:10, seq(from = 12, to = 20, by = 2)), 
+#'   networkType = "unsigned", verbose = 5)
+plot_softpowers <- function(counts, powers = c(1:10, seq(from = 12, to = 20, by = 2)),
+                            networkType = "unsigned", verbose = 5) {
 
   # Call the network topology analysis function
-  sft.tmp <- WGCNA::pickSoftThreshold(counts, powerVector = powers, verbose = verbose)
+  sft.tmp <- WGCNA::pickSoftThreshold(counts, powerVector = powers, networkType = networkType, verbose = verbose)
 
   # Scale-free topology fit index as a function of the soft-thresholding power
   plot(sft.tmp$fitIndices[,1],
-       -sign(sft.tmp$fitIndices[,3])*sft.tmp$fitIndices[,2],
-       xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
+       -sign(sft.tmp$fitIndices[,3]) * sft.tmp$fitIndices[,2],
+       xlab = "Soft Threshold (power)", ylab = "Scale Free Topology Model Fit,signed R^2", type = "n",
        main = paste("Scale independence"));
   text(sft.tmp$fitIndices[,1],
-       -sign(sft.tmp$fitIndices[,3])*sft.tmp$fitIndices[,2],
-       labels=powers,cex=0.9,col="red");
-  # this line corresponds to using an R^2 cut-off of h
-  abline(h=0.90,col="red")
+       -sign(sft.tmp$fitIndices[,3]) * sft.tmp$fitIndices[,2],
+       labels = powers, cex = 0.9, col = "red");
+  # this line corresponds to using an R^2 cut-off of 0.9
+  abline(h = 0.90, col = "red")
 
   # Mean connectivity as a function of the soft-thresholding power
   plot(sft.tmp$fitIndices[,1], sft.tmp$fitIndices[,5],
-       xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
+       xlab = "Soft Threshold (power)", ylab = "Mean Connectivity", type = "n",
        main = paste("Mean connectivity"))
-  text(sft.tmp$fitIndices[,1], sft.tmp$fitIndices[,5], labels=powers, cex=0.9,col="red")
+  text(sft.tmp$fitIndices[,1], sft.tmp$fitIndices[,5], labels = powers, cex = 0.9, col = "red")
 }
--- R/signedKME2.R
+++ R/signedKME2.R
@@ -3,11 +3,12 @@
 #' This is a wrapper for signedKME, with the additional step of reverting the gene names in the
 #' result to the input gene names. signedKME converts all special characters in the gene names to
 #' ".", and this causes name mismatches downstream.
-#' @param datExpr a data frame containing the gene expression data. Passed to signedKME.
-#' @param dateME a data frame containing module eigengenes. Passed to signedKME.
+#' @param datExpr a data frame containing the gene expression data, passed to signedKME
+#' @param datME a data frame containing module eigengenes, passed to signedKME
+#' @param ... additional optional arguments to be passed to signedKME
 #' @export
 #' @return A data frame, as returned by \code{signedKME}, with any "_" in the gene names of \code{datExpr} retained as "_" in the result.
-#' @usage \code{signedKME2(datExpr, datME, ...)}
+#' @usage signedKME2(datExpr, datME, ...)
 signedKME2 <- function(datExpr, datME, ...) {
   result <- WGCNA::signedKME(datExpr, datME, ...)
   rownames(result) <- colnames(datExpr)
